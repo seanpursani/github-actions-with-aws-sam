a61ca0ce838766b20b17c7a216252e1352575a29
Sean Pursani <seanpursani@gmail.com>
2022-05-20 13:31:56 +0200

    py testing

--- tests/tests.py
+++
@@ -1,8 +0,0 @@
-from unittest import TestCase
-
-class TestTesting(TestCase):
-    def test_always_passes(self):
-        self.assertTrue(True)
-
-    def test_always_fails(self):
-        self.assertTrue(False)
