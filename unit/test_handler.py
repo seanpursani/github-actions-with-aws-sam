b5c019bc1c6073bf0e0d79d6c100cc9153b551d4
Sean Pursani <seanpursani@gmail.com>
2022-05-20 13:47:20 +0200

    updates

--- unit/test_handler.py
+++ unit/test_handler.py
@@ -1,9 +1,7 @@
 import json
-
 import pytest
-
+from unittest import TestCase
 from hello_world import app
-
 
 @pytest.fixture()
 def apigw_event():
@@ -61,7 +59,7 @@ def apigw_event():
         "path": "/examplepath",
     }
 
-def test_lambda_handler(apigw_event, mocker):
+def test_lambda_handler(apigw_event):
 
     ret = app.lambda_handler(apigw_event, "")
     data = json.loads(ret["body"])
@@ -70,3 +68,10 @@ def test_lambda_handler(apigw_event, mocker):
     assert "message" in ret["body"]
     assert data["message"] == "hello world"
     # assert "location" in data.dict_keys()
+
+class TryTesting(TestCase):
+    def test_always_passes(self):
+        self.assertTrue(True)
+
+    def test_always_fails(self):
+        self.assertTrue(False)
